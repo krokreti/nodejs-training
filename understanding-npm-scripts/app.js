b8ce5a01dac948d1dbdbf7f3975bfa64b73ed2f7
davi <davipinheiro1993@gmail.com>
2023-02-09 09:22:17 -0300

    filtering routes

--- understanding-npm-scripts/app.js
+++ understanding-npm-scripts/app.js
@@ -9,10 +9,14 @@ const shopRoutes = require('./routes/shop')
 
 const bodyParser = require('body-parser')
 
-app.use(adminRoutes);
-app.use(shopRoutes);
-
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</>')
+})
 
 app.listen(3000)
--- understanding-npm-scripts/routes/admin.js
+++ understanding-npm-scripts/routes/admin.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 router.get("/add-product", (req, resp, next) => {
     console.log('in the middleware');
-    resp.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product<button/></form>');
+    resp.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Add Product<button/></form>');
 })
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
     console.log(req.body);
     console.log('entrou aqui')
+    // console.log(req);
     // console.log(req.body.title);
     res.redirect("/");
 })
