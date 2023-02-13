5d57dca579ebc2d1d75f7e96c6f87c7a8a3cd49a
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-02-13 18:44:11 -0300

    add mongodb schema

--- node-api/app.js
+++ node-api/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const path = require('path')
+
+const mongoose = require('mongoose')
+
 const bodyParser = require('body-parser');
 
 const feedRoute = require('./routes/feed');
@@ -7,6 +11,7 @@ const feedRoute = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json()) // => application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
@@ -18,4 +23,24 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoute);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+    console.log(error)
+    const status = error.statusCode || 500;
+    const message = error.message;
+    // var errorsArray = [];
+    // if (error.array) {
+    //     errorsArray = error.array
+    // }
+    res.status(status).json({ message: message })
+})
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb+srv://krokreti:caramujo123@cluster0.ijzar44.mongodb.net/messages?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(
+        err => {
+            console.log(err)
+        }
+    )
--- node-api/controllers/feed.js
+++ node-api/controllers/feed.js
@@ -1,38 +1,68 @@
+const { validationResult } = require('express-validator')
+
+const Post = require('../models/post')
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json(
-        {
-            posts: [
-                {
-                    _id: '1',
-                    title: "hamburguer",
-                    content: "thats a good hamburguer",
-                    imageUrl: 'images/yusuke.jpg',
-                    creator: {
-                        name: 'Davi'
-                    },
-                    createdAt: new Date()
-                }
-            ]
-        }
-    )
+    Post.find({}).then(result => {
+        res.status(200).json({
+            posts: result
+        });
+    })
+    // res.status(200).json(
+    //     {
+    //         posts: [
+    //             {
+    //                 _id: '1',
+    //                 title: "hamburguer",
+    //                 content: "thats a good hamburguer",
+    //                 imageUrl: 'images/yusuke.jpg',
+    //                 creator: {
+    //                     name: 'Davi'
+    //                 },
+    //                 createdAt: new Date()
+    //             }
+    //         ]
+    //     }
+    // )
 }
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.')
+        error.statusCode = 422;
+        error.array = errors.array();
+        throw error;
+        // return res.status(422).json({
+        //     message: 'Validation failed, entered data is incorrect.',
+        //     errors: errors.array()
+        // });
+    }
     const title = req.body.title;
     const content = req.body.content;
     console.log(`Creating post... title: ${title}`)
     //create data in db
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/yusuke.jpg',
+        creator: {
+            name: 'Davi'
+        },
+    });
 
-    res.status(201).json({
-        message: "Post created successfully!",
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {
-                name: 'Davi'
-            },
-            createdAt: new Date()
-        }
-    })
+    post
+        .save()
+        .then(result => {
+            res.status(201).json({
+                message: "Post created successfully!",
+                post: result
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 }
